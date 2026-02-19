aa75eb0ffe17ef388466b85b26cc989386345218
RafiMaulana22 <rafi.maulana211@gmail.com>
2026-02-20 03:05:29 +0700

    feat: Menambahkan Template pada Project

---
+++ resources/views/Layouts/template-admin.blade.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description"
+        content="Mofi admin is super flexible, powerful, clean &amp; modern responsive bootstrap 5 admin template with unlimited possibilities.">
+    <meta name="keywords"
+        content="admin template, Mofi admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="author" content="pixelstrap">
+    @include('Components.title')
+    <!-- Google font-->
+    <link rel="preconnect" href="https://fonts.googleapis.com/">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin="">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
+        rel="stylesheet">
+    @include('Components.styles')
+</head>
+
+<body>
+    <div class="loader-wrapper">
+        <div class="loader loader-1">
+            <div class="loader-outter"></div>
+            <div class="loader-inner"></div>
+            <div class="loader-inner-1"></div>
+        </div>
+    </div>
+    <!-- loader ends-->
+    <!-- tap on top starts-->
+    <div class="tap-top">
+        <i data-feather="chevrons-up"></i>
+    </div>
+    <!-- tap on tap ends-->
+    <!-- page-wrapper Start-->
+    <div class="page-wrapper compact-wrapper" id="pageWrapper">
+        <div class="page-header row">
+            <div class="header-logo-wrapper col-auto">
+                <div class="logo-wrapper">
+                    <a href="index.html">
+                        <img class="img-fluid for-light" src="{{ asset('') }}assets/images/logo/logo.png"
+                            alt="" />
+                        <img class="img-fluid for-dark" src="{{ asset('') }}assets/images/logo/logo_light.png"
+                            alt="" />
+                    </a>
+                </div>
+            </div>
+            <div class="col-4 col-xl-4 page-title">
+                <h4 class="f-w-700">Default dashboard</h4>
+                <nav>
+                    <ol class="breadcrumb justify-content-sm-start align-items-center mb-0">
+                        <li class="breadcrumb-item">
+                            <a href="index.html">
+                                <i data-feather="home"> </i>
+                            </a>
+                        </li>
+                        <li class="breadcrumb-item f-w-400">Dashboard</li>
+                        <li class="breadcrumb-item f-w-400 active">Default</li>
+                    </ol>
+                </nav>
+            </div>
+            <!-- Page Header Start-->
+            @include('Components.header')
+            <!-- Page Header Ends -->
+        </div>
+        <!-- Page Body Start-->
+        <div class="page-body-wrapper">
+            <!-- Page Sidebar Start-->
+            @include('Components.sidebar')
+            <!-- Page Sidebar Ends-->
+            <div class="page-body">
+                <!-- Container-fluid starts-->
+                @yield('content')
+                <!-- Container-fluid Ends-->
+            </div>
+            <!-- footer start-->
+            @include('Components.footer')
+        </div>
+    </div>
+    @include('Components.script')
+</body>
+
+</html>
