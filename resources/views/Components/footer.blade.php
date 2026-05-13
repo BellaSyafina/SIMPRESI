ab9df3fd717862150d758a99f23390d846ead993
BellaSyafina <bellaarumsyafina04@gmail.com>
2026-05-13 21:34:48 +0700

    SIMPRESI: Memperbaiki Tampilan dibagian Footer

--- resources/views/Components/footer.blade.php
+++ resources/views/Components/footer.blade.php
@@ -1,8 +1,30 @@
 <footer class="footer">
     <div class="container-fluid">
-        <div class="row">
+
+        <div class="d-flex justify-content-between align-items-center flex-wrap">
+
+            <!-- KIRI: LOGO & IDENTITAS -->
+            <div class="d-flex align-items-center mb-2">
+                <img src="{{ asset('assets/images/smpn.png') }}" alt="Logo Sekolah" class="me-3"
+                    style="width:80px; height:80px; object-fit:contain;">
+
+                <div>
+                    <div class="fw-bold">
+                        SMPN 2 SARONGGI
+                    </div>
+                    <div class="fw-medium">
+                        Dusun Katapang, Panggulan, Kebundadap Timur, Kec. Saronggi, Kabupaten Sumenep, Jawa Timur
+                        69467<br>
+                        Telp: 0819-0220-0300
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- COPYRIGHT -->
+       <div class="row">
             <div class="col-md-12 footer-copyright d-flex flex-wrap align-items-center justify-content-between">
-                <p class="mb-0 f-w-600">Copyright <span class="year-update"> </span> © Bella Syafina </p>
+                <p class="mb-0 f-w-600">Copyright <span class="year-update"> </span> © SIMPRESI - Bella Syafina </p>
                 <p class="mb-0 f-w-600">Hand crafted & made with
                     <svg class="footer-icon">
                         <use href="{{ asset('') }}assets/svg/icon-sprite.svg#footer-heart"> </use>
@@ -10,5 +32,6 @@
                 </p>
             </div>
         </div>
+
     </div>
 </footer>
